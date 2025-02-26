91213e083c0e700d8cb20abf318c06c95e8cbb5b
JakwanAja <muhzakwanalfaris195@gmail.com>
2025-02-26 09:31:05 +0700

    Add interface .xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.wanjakwan.a234311019_loginpage"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.wanjakwan.a234311019_loginpage"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
